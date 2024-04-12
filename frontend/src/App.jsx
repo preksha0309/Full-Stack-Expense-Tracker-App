bb871853421158319f928acb0e1603c3b7133400
Preksha Gupta <prekshagupta03@gmail.com>
2024-04-12 16:00:03 +0530

    Ui folder structure and tailwind css setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold underline bg-stone-400">
+      Hello world!
+    </h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
